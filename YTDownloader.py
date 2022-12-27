c33ca59641117a022c70ad8229c6053dd406c1dd
ali0248513 <89481510+ali0248513@users.noreply.github.com>
2022-12-27 16:52:15 +0500

    Update YTDownloader.py

--- YTDownloader.py
+++ YTDownloader.py
@@ -2,19 +2,20 @@ from pytube import YouTube
 from tkinter import *
 import ctypes as ct
 
+#Initialize the window
 win = Tk()
 win.geometry("600x600")
 win.title('YT Video Downloader')
 win.resizable(width=False, height=False)
-#win.configure(bg= 'honeydew2')
 
 c = Canvas(win, bg="gray16")
+#Location for the background image
 filename=PhotoImage(file="D:\\Sublime Text 3\\PROJECTS\\YTDownloader\\12.png")
 background_label= Label(win, image= filename)
 background_label.place(x=0,y=0,relwidth=1, relheight=1)
 
-def get_link():
-	yt = link.get()
+def get_link():                                 #This Function sotres the Link inputed by the user then this link is used to get the title
+	yt = link.get()                         #of the video by ".title" and then download it using ".streams.get_highest_resolution()" function.
 	yt = YouTube(yt)
 	txt =yt.title
 	yd = yt.streams.get_highest_resolution()
@@ -31,22 +32,22 @@ def get_link():
 	d_path = Entry(win, width= 48,fg = '#E6B3CD', bg = '#10222F')
 	d_path.grid(column=1 , row=2)
 	
-	def d_p():
+	def d_p():                            #This Function is used to take the download path input from the user and download the video in that location
 		yd.download(d_path.get())
 
 	d_button = Button(text= 'Go!' ,font= (20), command= d_p, fg= 'green', bg = '#10222F')
 	d_button.grid(column=2 , row=2, sticky= 'E')
 
-
+#Prompting the user to enter the Link for the video
 link_l = Label(text='Enter Link' ,font= 20,fg = 'red', bg = '#10222F')
 link_l.grid(column=0 , row=0, sticky = 'w')
 
+#Taking the input Link from the user
 link = Entry(win, width= 48,fg = '#E6B3CD', bg = '#10222F')
 link.grid(column=1 , row=0)
 
-
-l_button= Button(text="Enter" , font=(20),fg= 'green', bg = '#10222F', command= get_link)
+#Button to indicate that the user has finished entering the link
+l_button= Button(text="Enter" , font=(20),fg= 'green', bg = '#10222F', command= get_link)#Go to line 17 for details
 l_button.grid(column= 2, row= 0, sticky= 'E')
-
 
 win.mainloop()
